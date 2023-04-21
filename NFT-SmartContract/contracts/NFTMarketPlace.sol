18188332b423416393f4a82f3f082e600c2324fd
vishal <vishaltyagi.dev098@gmail.com>
2023-04-21 18:30:08 +0530

    contract started: NFTMarketPlace

--- NFT-SmartContract/contracts/NFTMarketPlace.sol
+++ NFT-SmartContract/contracts/NFTMarketPlace.sol
@@ -7,3 +7,33 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/ERC721/ERC721.sol";
 
 import "hardhat/console.sol";
+
+// Contract-1 : NFT Selling and Buying
+contract NFTMarketPlace is ERC721URIStorage{
+  // Making counters for the NFT
+  using Counters for Counters.Counter;
+
+  Counters.Counter private _tokenIds;
+  Counters.Counter private _itemsSold;
+  address payable owner;
+
+  // Making the NFT market item structure
+  mapping(uint256=>MarketItem) private idMarketItem;
+  struct MarketItem{
+    uint256 tokenId;
+    address payable seller;
+    address payable owner;
+    uint256 price;
+    bool sold;
+  }
+
+  // Event: Stores the information of created NFT
+  event idMarketItemCreated(
+    uint256 indexed tokenId,
+    address seller,
+    address owner,
+    uint256 price,
+    bool sold
+  )
+
+}
