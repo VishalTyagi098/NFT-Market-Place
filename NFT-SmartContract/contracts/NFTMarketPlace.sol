67e4765b2f2cb87083e57d1e0c4570e9644b8a6c
vishal <vishaltyagi.dev098@gmail.com>
2023-04-21 18:59:21 +0530

    initial states declared

--- NFT-SmartContract/contracts/NFTMarketPlace.sol
+++ NFT-SmartContract/contracts/NFTMarketPlace.sol
@@ -15,6 +15,7 @@ contract NFTMarketPlace is ERC721URIStorage{
 
   Counters.Counter private _tokenIds;
   Counters.Counter private _itemsSold;
+  uint256 listingPrice=0.0015 ether;
   address payable owner;
 
   // Making the NFT market item structure
@@ -34,6 +35,36 @@ contract NFTMarketPlace is ERC721URIStorage{
     address owner,
     uint256 price,
     bool sold
-  )
+  );
+
+  // Modifier: for updateListingPrice function 
+  modifier onlyOwner{
+    require(
+      msg.sender==owner,"Only owner of the marketplace can change the listing price"
+    );
+    _;
+  }
+
+  // Constructor: Initialise states values
+  constructor() ERC721("NFT Metaverse Token","MYNFT"){
+    owner==payable(msg.sender);
+  }
+
+  // Function: to update the listing price for the NFT that the NFT owner will pay us for listing an item on our market place
+  function updateListingPrice(uint256 _listingPrice) public payable onlyOwner{
+    listingPrice=_listingPrice;
+  }
+  // Function: to get the listing price data of an NFT
+  function getListingPrice() public view returns (uint256){
+    return listingPrice;
+  }
+
+
+
+
+
+
+  // Function: Create NFT Token Function
+
 
 }
