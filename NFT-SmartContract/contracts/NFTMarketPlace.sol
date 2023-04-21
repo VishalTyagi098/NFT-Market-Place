d8327453ffb67e469a14288ad7312747d9535a0b
vishal <vishaltyagi.dev098@gmail.com>
2023-04-21 10:58:39 +0530

    imported packages

---
+++ NFT-SmartContract/contracts/NFTMarketPlace.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+// INTERNAL IMPORT FOR THE NFT OPENZEPPELINE
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/ERC721/ERC721.sol";
+
+import "hardhat/console.sol";
